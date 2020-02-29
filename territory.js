fb410c9bd3aa296b788d90d473786ae1c0f05804
Abhijay Sahay <abhijay.sahay@student.manchester.ac.uk>
2020-02-29 14:16:37 +0000

    Added path to territory class

--- territory.js
+++ territory.js
@@ -1,6 +1,7 @@
-var Territory = function(name, continent) {
+var Territory = function(name, continent, path) {
     this.name = name;
     this.continent = continent;
+    this.path = path;
     this.owner = null;
     
     // every territory gets 3 troops in the beginning
